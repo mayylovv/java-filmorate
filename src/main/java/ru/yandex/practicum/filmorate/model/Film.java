da35d3245efa9b4af2c97516b47d9a33b36a67dd
mayylovv <119136126+mayylovv@users.noreply.github.com>
2023-09-07 01:39:42 +0300

    third time

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,18 +8,12 @@ import javax.validation.constraints.*;
 @Data
 public class Film {
     private int id;
-
     @NotNull(message = "Название не может отсутствовать")
     @NotEmpty(message = "Не может быть пустым название")
     private final String name;
-
-    @NotBlank(message = "Описание фильма не может быть пустым")
     @Size(min = 1, max = 200, message = "Максимальная длина описания — 200 символов")
     private final String description;
-
     private final LocalDate releaseDate;
-
-    @NotNull
     @Positive(message = "Продолжительность фильма должна быть положительной")
     private final int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -9,18 +9,12 @@ import javax.validation.constraints.*;
 @Data
 public class User {
     private int id;
-
-    @NotNull(message = "Адрес электронной почты не может быть пустым")
-    @NotBlank
     @Email(message = "В адресе электронной почты ошибка")
     private final String email;
-
     @NotBlank(message = "Логин не может быть пустым и содержать пробелы")
     @NotEmpty(message = "Логин не может быть пустым")
     private final String login;
-
     private String name;
-
     @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private final LocalDate birthday;
 }
